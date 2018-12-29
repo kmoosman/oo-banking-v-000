8d1ccfc285f9ca8637fdb265ca2c919635bb8137
Katie Moosman <moosman.katie@gmail.com>
2018-12-29 05:18:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,9 +17,10 @@ class Transfer
   end
   
   def execute_transaction
-    sender.balance = sender.balance - amount
-    receiver.balance = receiver.balance + amount
-    @status = "complete"
+    if status = "pending"
+      sender.balance = sender.balance - amount
+      receiver.balance = receiver.balance + amount
+      @status = "complete"
   end
   
   
