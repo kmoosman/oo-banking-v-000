0df7f6049ca7efbf51e06971741519f83a0a680d
Katie Moosman <moosman.katie@gmail.com>
2018-12-29 05:23:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,9 +18,10 @@ class Transfer
   
   def execute_transaction
     if valid? && status == "pending"
-      sender.balance = sender.balance - amount
-      receiver.balance = receiver.balance + amount
-      @status = "complete"
+      if amount < sender.balance 
+        sender.balance = sender.balance - amount
+        receiver.balance = receiver.balance + amount
+        @status = "complete"
     else 
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
