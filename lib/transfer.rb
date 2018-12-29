e63b719bc31fe1f2053aa76d03b69dd5fa6e78bc
Katie Moosman <moosman.katie@gmail.com>
2018-12-29 05:25:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,7 +32,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if @status = "complete"
+    if @status == "complete"
       sender.balance = sender.balance + amount
       receiver.balance = receiver.balance - amount
       @status = "reversed"
